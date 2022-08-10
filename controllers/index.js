03e3130d3ae7d76b23c45699c9547e1e669d8e32
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-10 14:29:08 -0700

    fixed a couple bugs that were breaking the server. Now server fixed but front end pages are broken. Currently working on those

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homejs');
-const dashboardRoutes = require('./dashboardjs');
+const homeRoutes = require('./home.js');
+const dashboardRoutes = require('./dashboard.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
