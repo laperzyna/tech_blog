8e004c7b3079159bf4b852afcdef2d0ae7d551d4
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-10 13:41:42 -0700

    initialized cookie,secret, sess

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// TECH BLOG - CHECKED, SAME AS REFERENCE
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -12,6 +11,28 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Tech blog secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 
 app.listen(PORT, () => {
